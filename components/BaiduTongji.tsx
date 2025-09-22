8b5060b742e58008c67c10e9fffde0613eb2add5
deanmmm42 <deanmmm42@gmail.com>
2025-09-22 18:17:11 +0800

    feat 更新百度统计id

--- components/BaiduTongji.tsx
+++ components/BaiduTongji.tsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 
-const BAIDU_TONGJI_ID = '328ba2b9c162a3e1e011494cc35f3c39'
+const BAIDU_TONGJI_ID = '40fb4d247ad6803abcc46358c1cc3d67'
 
 export default function BaiduTongji() {
   return (
